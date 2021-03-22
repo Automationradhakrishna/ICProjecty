725a9f38e2763365e791d26c90caa0b56a11f113
rxs764 <rxs764@USTTRVEFXG307LT.eis.equifax.com>
2021-03-22 20:37:49 +0530

    First Commit

---
+++ ICProject/src/main/java/ICProject/ICProject/App.java
@@ -0,0 +1,13 @@
+package ICProject.ICProject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
